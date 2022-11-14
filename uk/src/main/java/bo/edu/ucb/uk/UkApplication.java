8e8b6f73876aae8cb4215b19f0cf96f39391767a
Lindsay Uria <lindsay.karla.uria.valdivia@gmail.com>
2022-11-14 16:33:21 -0400

    Creando archivos base del backend

---
+++ uk/src/main/java/bo/edu/ucb/uk/UkApplication.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.uk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UkApplication.class, args);
+	}
+
+}
---
+++ uk/src/test/java/bo/edu/ucb/uk/UkApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.uk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
