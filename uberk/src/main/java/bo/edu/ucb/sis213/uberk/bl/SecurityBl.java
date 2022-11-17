fb59d028733ac037fbb6337a679402cf65f57fc2
Lindsay Uria <lindsay.karla.uria.valdivia@gmail.com>
2022-11-16 22:38:44 -0400

    Funcion para generar token y refresh

--- uberk/src/main/java/bo/edu/ucb/sis213/uberk/bl/SecurityBl.java
+++ uberk/src/main/java/bo/edu/ucb/sis213/uberk/bl/SecurityBl.java
@@ -6,8 +6,13 @@ import bo.edu.ucb.sis213.uberk.dto.AuthReqDto;
 import bo.edu.ucb.sis213.uberk.dto.AuthResDto;
 import bo.edu.ucb.sis213.uberk.dto.UserDto;
 import bo.edu.ucb.sis213.uberk.entity.UkUser;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
 import org.springframework.stereotype.Service;
 import at.favre.lib.crypto.bcrypt.BCrypt;
+
+import java.util.Date;
 
 
 //Logica de negocio
@@ -45,20 +50,56 @@ public class SecurityBl {
         System.out.println("Comenzando proceso de autenticacion con:"+credentials);
         String currentPasswordInBCrypt = userDao.findByUsernameAndPassword(credentials.username());
         System.out.println("Se obtuvo la siguiente contraseña de bbdd: " + currentPasswordInBCrypt);
-        //consulto si los passwords coenciden
-        System.out.println("Se procede a comparar contrasenas");
-        BCrypt.Result bcryptresult = BCrypt.verifyer().verify(credentials.password().toCharArray(),currentPasswordInBCrypt);
+
+
         //currentPasswordInBCrypt.equals(credentials.password())
+
         if (currentPasswordInBCrypt != null){
-            //proceso a generar el token
-            System.out.println("Se genera el token");
-            result.setToken("TEST TOKEN");
-            result.setRefresh("TEST REFRESH TOKEN");
+            System.out.println("Se procede a comparar contrasenas");
+            BCrypt.Result bcryptresult = BCrypt.verifyer().verify(credentials.password().toCharArray(),currentPasswordInBCrypt);
+            //consulto si los passwords coenciden
+            if (bcryptresult.verified){
+                //proceso a generar el token
+                System.out.println("Las contrasenas generan token");
+                //result.setToken("TEST TOKEN");
+                //result.setRefresh("TEST REFRESH TOKEN");
+                result = generateTokenJet(credentials.username(), 300, new String[]{"admin","user"});
+            }else{
+                System.out.println("Las contrasenas no coenciden");
+                throw new RuntimeException("Forbiden the password");
+            }
         }else {
-            System.out.println("Las contrasenas no coenciden");
+            System.out.println("El usuario no existe");
             throw new RuntimeException("Forbiden the password");
         }
         return  result;
+    }
+    /**Este metodo genera tokens JWT*/
+    private AuthResDto generateTokenJet(String subject, int expirationTimeInSeconds, String[] roles){
+        AuthResDto result = new AuthResDto();
+        //generamos el token principal
+        try{
+            Algorithm algorithm = Algorithm.HMAC256("Neko");
+            String token = JWT.create()
+                    .withIssuer("ucb")
+                    .withSubject(subject)
+                    .withArrayClaim("roles",roles)
+                    .withClaim("refresh", false)
+                    .withExpiresAt(new Date(System.currentTimeMillis()+(expirationTimeInSeconds *  1000)))
+                    .sign(algorithm);
+            result.setToken(token);
+            String refreshToken = JWT.create()
+                    .withIssuer("ucb")
+                    .withSubject(subject)
+                    .withClaim("refresh", true)
+                    .withExpiresAt(new Date(System.currentTimeMillis()+(expirationTimeInSeconds *  1000 * 2)))
+                    .sign(algorithm);
+            result.setToken(token);
+            result.setRefresh(refreshToken);
+        }catch (JWTCreationException exception){
+            throw new RuntimeException("Error al generar token",exception);
+        }
+        return result;
     }
 
 }
--- uberk/src/test/java/bo/edu/ucb/sis213/uberk/unit/SecurityBlTest.java
+++ uberk/src/test/java/bo/edu/ucb/sis213/uberk/unit/SecurityBlTest.java
@@ -13,18 +13,20 @@ public class SecurityBlTest {
     @Test
     void successfulAuthentication() {
 //        // Implementación falsa
-          UserDao userDao = Mockito.mock(UserDao.class);
+//          UserDao userDao = Mockito.mock(UserDao.class);
+
+          //AuthResDto response = securityBl.authenticate(new AuthReqDto("admin","123456"));
 //        // Nosotros sabemos que para esta prueba se necesita:
 //        userDao.findByUsernameAndPassword(credentials.username());
 //        // Debido a que es prueba unitaria debemos crear el comportamiento de lo que va a hacer el mock
 //        // Cuando se llame a findByUsernameAndPassword con el argumento "admin"
-//        Mockito.when(userDao.findSecretByUsername("admin"))
-//                .thenReturn("$2a$12$So1a4kRWCh9USHBfUgs1NOtwUCNXLFvuSmfccBOLe.rPxsy0.LGC6");
-//        SecurityBl securityBl = new SecurityBl(mrUserDao);
-//        AuthResDto response = securityBl.authenticate(new AuthReqDto("admin", "123456"));
-//        Assertions.assertNotNull(response); // Que la respuesta no sea nula
-//        Assertions.assertNotNull(response.getToken()); // Que el token no sea nulo
-//        Assertions.assertNotNull(response.getRefresh()); // Que el refresh token no sea nulo
+//          Mockito.when(userDao.findSecretByUsername("admin"))
+//                  .thenReturn("$2a$12$pigNJzj3QRUXc6LiG7YF2Oi.oC3fEPFB4sOkSm1nAmp6UzWlbZhEm");
+//          SecurityBl securityBl = new SecurityBl(userDao);
+//          AuthResDto response = securityBl.authenticate(new AuthReqDto("admin", "123456"));
+//          Assertions.assertNotNull(response); // Que la respuesta no sea nula
+ //         Assertions.assertNotNull(response.getToken()); // Que el token no sea nulo
+ //         Assertions.assertNotNull(response.getRefresh()); // Que el refresh token no sea nulo
     }
 }
 
